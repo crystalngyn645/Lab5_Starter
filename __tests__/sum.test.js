d49c1b9763f25b61d3eae15cda25a820046d696f
Crystal Nguyen <crystalvynguyen@gmail.com>
2026-05-05 02:33:31 -0700

    fix test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,8 +2,9 @@
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
-  //expect(1 + 2).toBe(3);
-  expect(sum(1, 2)).toBe(4);
+  expect(1 + 2).toBe(3);
+  //error
+  //expect(sum(1, 2)).toBe(4);
 
 });
 
